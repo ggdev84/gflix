f7a56f0453230485ffa5f28b6108ba62a0d1098f
gorkemgul <64042667+gorkemgul@users.noreply.github.com>
2020-04-20 23:53:20 +0200

    Update menu.js

--- menu.js
+++ menu.js
@@ -4,7 +4,7 @@ function Menu(){
 
     // L'aide de Gflix.
     var help = ()=>{
-        alert("Bienvenue dans l'aide de GFlix\n\nRecherchez une vidéo, cliquez dessus pour la regarder.\n\nVous pouvez également la commenter et voir les autres commentaires sans vous inscrire.")
+        alert("Bienvenue dans l'aide de GFlix\n\nRecherchez une vidéo et/ou cliquez sur l'une des vidéos pour la regarder.\n\nVous pouvez également la commenter et voir les autres commentaires sans vous inscrire.")
     }
 
     // Le menu tout en haut, aussi bien sur mobile que PC.
